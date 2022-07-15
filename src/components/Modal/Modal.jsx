da8105525949119a5311fb1b9611e33a3a4b2358
BalVil <balutavili@gmail.com>
2022-07-15 03:46:00 +0300

    Update Modal.jsx

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,7 +1,6 @@
 import { Component } from 'react';
 import { createPortal } from 'react-dom';
 import { Overlay, ImageModal } from './Modal.styled';
-import { ImageGalleryItem } from 'components/ImageGalleryItem/ImageGalleryItem';
 
 const modalRoot = document.querySelector('#modal-root');
 
